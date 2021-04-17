6b6926cee200c347004c18671a6a08d15cfce9a6
MirhanTanriverdi <taha.tanriverdi@code.berlin>
2021-04-18 00:06:37 +0300

    navbar

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const { app } = require("firebase-admin");
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
